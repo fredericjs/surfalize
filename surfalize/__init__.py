67f3e3f8bd70127359a9e4e9c727c60bfb60dff9
Frederic Schell <frederic.schell@iws.fraunhofer.de>
2023-11-08 09:01:05 +0000

    Moved Batch to separate module

--- surfalize/__init__.py
+++ surfalize/__init__.py
@@ -1,1 +1,2 @@
-from .surface import Surface, Profile, Batch
+from .surface import Surface, Profile
+from .bacth import Batch
--- surfalize/surface.py
+++ surfalize/surface.py
@@ -17,14 +17,6 @@ from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
 from scipy.ndimage import rotate as scipy_rotate
 
-try:
-    # Optional import
-    from tqdm import tqdm
-except ImportError:
-    logger.info('tqdm not found, no progessbars will be shown.')
-    # If tqdm is not defined, replace with dummy function
-    tqdm = lambda x, *args, **kwargs: x
-
 # Custom imports
 from . fileloader import load_file
 try:
@@ -59,52 +51,6 @@ def _period_from_profile(profile):
     prominences_sorted = prominences[sorted_indices]
     period = 1/np.abs(freq[peaks_sorted[0]])
     return period
-
-#TODO batch image export
-class Batch:
-    
-    def __init__(self, filepaths):
-        """
-        Initializies the Batch object from filepaths to topography files.
-
-        Parameters
-        ----------
-        filepaths: list of filepaths
-        """
-        self._filepaths = [Path(file) for file in filepaths]
-        self._load_surfaces()
-        
-    def _load_surfaces(self):
-        self._surfaces = dict()
-        for file in tqdm(self._filepaths, desc='Loading files'):
-            self._surfaces[file] = Surface.load(file)
-            
-    def fill_nonmeasured(self, mode='nearest'):
-        for surface in tqdm(self._surfaces.values(), desc='Filling non-measured points'):
-            surface.fill_nonmeasured(mode=mode, inplace=True)
-        return self
-            
-    def level(self):
-        for surface in tqdm(self._surfaces.values(), desc='Leveling'):
-            surface.level(inplace=True)
-        return self
-            
-    def filter(self, cutoff, *, mode, cutoff2=None, inplace=False):
-        for surface in tqdm(self._surfaces.values(), desc='Filtering'):
-            surface.filter(cutoff, mode=mode, cutoff2=None, inplace=True)
-        return self
-
-    def roughness_parameters(self, parameters=None):
-        if parameters is None:
-            parameters = list(Surface.AVAILABLE_PARAMETERS)
-        df = pd.DataFrame({'filepath': [file.name for file in self._filepaths]})
-        df = df.set_index('filepath')
-        df[list(parameters)] = np.nan
-        for file, surface in tqdm(self._surfaces.items(), desc='Calculating parameters'):
-            results = surface.roughness_parameters(parameters)
-            for k, v in results.items():
-                df.loc[file.name][k] = v
-        return df.reset_index()
 
 
 class Profile:
