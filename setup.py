ed05d20677b230d4543b6d45f883e321d7c03fd1
Frederic Schell <frederic.schell@iws.fraunhofer.de>
2023-11-11 13:31:22 +0100

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 
-
-# Define your Cython extension module
 extensions = [
     Extension(
-        'surfalize.calculations',  # Replace with the path to your Cython module
-        sources=['surfalize/calculations.pyx'],  # Replace with the path to your .pyx file
+        'surfalize.calculations',
+        sources=['surfalize/calculations.pyx'],
     ),
 ]
 
-
-# Define your package's metadata
 setup(
     name='surfalize',
     version='0.0.1',
@@ -19,21 +15,21 @@ setup(
     author='Frederic Schell',
     author_email='frederic.schell@iws.fraunhofer.de',
     packages=find_packages(),
-##    classifiers=[
-##        'Programming Language :: Python',
-##        'Programming Language :: Python :: 3',
-##        'Programming Language :: Python :: 3.6',
-##        'Programming Language :: Python :: 3.7',
-##        'Programming Language :: Python :: 3.8',
-##        'Programming Language :: Python :: 3.9',
-##        'Programming Language :: Python :: 3.10',
-##        'Programming Language :: Python :: 3.11',
-##    ],
-##    install_requires=[
-##        'numpy>=1.18.1',
-##        'matplotlib>=3.1.1',
-##        'pandas>=1.0.1',
-##        'scipy>=1.4.1',
-##    ],
+    classifiers=[
+       'Programming Language :: Python',
+       'Programming Language :: Python :: 3',
+       'Programming Language :: Python :: 3.6',
+       'Programming Language :: Python :: 3.7',
+       'Programming Language :: Python :: 3.8',
+       'Programming Language :: Python :: 3.9',
+       'Programming Language :: Python :: 3.10',
+       'Programming Language :: Python :: 3.11',
+    ],
+    install_requires=[
+       'numpy>=1.18.1',
+       'matplotlib>=3.1.1',
+       'pandas>=1.0.1',
+       'scipy>=1.4.1',
+    ],
     ext_modules=cythonize(extensions)
 )
