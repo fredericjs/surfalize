950f3c0bd52c21708872175319ee981913ee4862
Frederic Schell <frederic.schell@iws.fraunhofer.de>
2023-11-28 17:04:30 +0100

    Reworked Batch processing
    Batch now doesn't load the surfaces on initialization. Instead, operations and parameters
    are registered and are only applied to the surfaces upon calling of the Batch.execute method.
    The advantage is firstly, that not all surface files have to be loaded into memory at once,
    which could be a´n issue for larger batch size. Secondly, this is the basis for sharing the
    processing load among different cores using multiprocesing. Right now, multiprocessing doesn't
    work yet, this remains to be solved. Moreover, a method for calculation of all parameters at
    once as well as the merging of the data into a dataframe needs to be reimplemented.

--- surfalize/batch.py
+++ surfalize/batch.py
@@ -1,3 +1,4 @@
+from multiprocessing import Pool
 from pathlib import Path
 import logging
 logger = logging.getLogger(__name__)
@@ -12,7 +13,29 @@ except ImportError:
     logger.info('tqdm not found, no progessbars will be shown.')
     # If tqdm is not defined, replace with dummy function
     tqdm = lambda x, *args, **kwargs: x
-    
+
+class _Operation:
+
+    def __init__(self, identifier, args=None, kwargs=None):
+        self.identifier = identifier
+        self.args = tuple() if args is None else args
+        self.kwargs = dict() if kwargs is None else kwargs
+
+    def execute_on(self, surface):
+        method = getattr(surface, self.identifier)
+        method(*self.args, **self.kwargs)
+
+class _Parameter:
+
+    def __init__(self, identifier, args=None, kwargs=None):
+        self.identifier = identifier
+        self.args = tuple() if args is None else args
+        self.kwargs = dict() if kwargs is None else kwargs
+
+    def calculate_from(self, surface):
+        method = getattr(surface, self.identifier)
+        return method(*self.args, **self.kwargs)
+
 
 #TODO batch image export
 class Batch:
@@ -40,57 +63,108 @@ class Batch:
         """
         self._filepaths = [Path(file) for file in filepaths]
         self._additional_data = additional_data
-        self._load_surfaces()
+        self._operations = []
+        self._parameters = []
         
-    def _load_surfaces(self):
-        """
-        Loads each files into a surfalize.Surface object.
-        """
-        self._surfaces = dict()
-        for file in tqdm(self._filepaths, desc='Loading files'):
-            self._surfaces[file] = Surface.load(file)
+    def execute(self, multiprocessing=False):
+        def task(filepath, operations, parameters):
+            surface = Surface.load(filepath)
+            for operation in operations:
+                operation.execute_on(surface)
+            results = dict()
+            for parameter in parameters:
+                result = parameter.calculate_from(surface)
+                results[parameter.identifier] = result
+            return results
+
+        results = []
+        if multiprocessing:
+            with Pool() as pool:
+                for result in pool.map(task, self._filepaths):
+                    results.append(result)
+            return results
+
+        for filepath in self._filepaths:
+            results.append(task(filepath))
+        return results
+
+    def zero(self):
+        operation = _Operation('zero', kwargs=dict(inplace=True))
+        self._operations.append(operation)
+        return self
+
+    def center(self):
+        operation = _Operation('center', kwargs=dict(inplace=True))
+        self._operations.append(operation)
+        return self
             
     def fill_nonmeasured(self, mode='nearest'):
         """
         Calls the 'fill_nonmeasured' method of each surface in the batch with the inplace argument
         specified as True.
         """
-        for surface in tqdm(self._surfaces.values(), desc='Filling non-measured points'):
-            surface.fill_nonmeasured(mode=mode, inplace=True)
+        operation = _Operation('fill_nonmeasured', kwargs=dict(mode=mode, inplace=True))
+        self._operations.append(operation)
         return self
             
     def level(self):
-        for surface in tqdm(self._surfaces.values(), desc='Leveling'):
-            surface.level(inplace=True)
+        operation = _Operation('level', kwargs=dict(inplace=True))
+        self._operations.append(operation)
         return self
             
     def filter(self, cutoff, *, mode, cutoff2=None, inplace=False):
-        for surface in tqdm(self._surfaces.values(), desc='Filtering'):
-            surface.filter(cutoff, mode=mode, cutoff2=None, inplace=True)
+        operation = _Operation('filter', args=(cutoff, ), kwargs=dict(mode=mode, cutoff2=cutoff2, inplace=True))
+        self._operations.append(operation)
         return self
     
     def rotate(self, angle):
-        for surface in tqdm(self._surfaces.values(), desc='Rotating'):
-            surface.rotate(angle, inplace=True)
+        operation = _Operation('rotate', args=(angle,), kwargs=dict(inplace=True))
+        self._operations.append(operation)
         return self
     
     def align(self):
-        for surface in tqdm(self._surfaces.values(), desc='Aligning'):
-            surface.align(inplace=True)
+        operation = _Operation('align', kwargs=dict(inplace=True))
+        self._operations.append(operation)
         return self
 
-    def roughness_parameters(self, parameters=None):
-        if parameters is None:
-            parameters = list(Surface.AVAILABLE_PARAMETERS)
-        df = pd.DataFrame({'filename': [file.name for file in self._filepaths]})
-        df = df.set_index('filename')
-        df[list(parameters)] = np.nan
-        for file, surface in tqdm(self._surfaces.items(), desc='Calculating parameters'):
-            results = surface.roughness_parameters(parameters)
-            for k, v in results.items():
-                df.loc[file.name][k] = v
-        df = df.reset_index()
-        if self._additional_data is None:
-            return df
-        dfin = pd.read_excel(self._additional_data)
-        return pd.merge(dfin, df, on='filename')
+    def zoom(self, factor):
+        operation = _Operation('zoom', args=(factor,), kwargs=dict(inplace=True))
+        self._operations.append(operation)
+        return self
+
+    def __getattr__(self, attr):
+        # This is probably a questionable implementation
+        # The call to getattr checks if the attribute exists in this class. If not, it checks whether the attribute
+        # is part of the available roughness parameters of the surfalize.Surface class. If it is not, it raises
+        # the original AttributeError again. If it is a parameter of surfalize.Surface class though, it constructs
+        # a dummy method that is returned to the caller which, instead of calling the actual method from the Surface
+        # class, registers the parameter with the corresponding arguments in this class for later execution.
+        try:
+            return self.__dict__[attr]
+        except KeyError:
+            if attr not in Surface.AVAILABLE_PARAMETERS:
+                raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{attr}'")
+        def parameter_dummy_method(*args, **kwargs):
+            parameter = _Parameter(attr, args=args, kwargs=kwargs)
+            self._parameters.append(parameter)
+            return self
+
+        return parameter_dummy_method
+
+    # To be reimplemented
+
+    # def roughness_parameters(self, parameters=None):
+    #     if parameters is None:
+    #         parameters = list(Surface.AVAILABLE_PARAMETERS)
+    #     df = pd.DataFrame({'filename': [file.name for file in self._filepaths]})
+    #     df = df.set_index('filename')
+    #     df[list(parameters)] = np.nan
+    #     for file, surface in tqdm(self._surfaces.items(), desc='Calculating parameters'):
+    #         results = surface.roughness_parameters(parameters)
+    #         for k, v in results.items():
+    #             df.loc[file.name][k] = v
+    #     df = df.reset_index()
+    #     if self._additional_data is None:
+    #         return df
+    #     dfin = pd.read_excel(self._additional_data)
+    #     return pd.merge(dfin, df, on='filename')
