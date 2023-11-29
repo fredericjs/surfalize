04a9d02450525e83a1867e106729155dbb078294
Frederic Schell <frederic.schell@iws.fraunhofer.de>
2023-11-29 12:06:55 +0100

    Added tqdm dependency

--- setup.py
+++ setup.py
@@ -16,20 +16,21 @@ setup(
     author_email='frederic.schell@iws.fraunhofer.de',
     packages=find_packages(),
     classifiers=[
-       'Programming Language :: Python',
-       'Programming Language :: Python :: 3',
-       'Programming Language :: Python :: 3.6',
-       'Programming Language :: Python :: 3.7',
-       'Programming Language :: Python :: 3.8',
-       'Programming Language :: Python :: 3.9',
-       'Programming Language :: Python :: 3.10',
-       'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     install_requires=[
-       'numpy>=1.18.1',
-       'matplotlib>=3.1.1',
-       'pandas>=1.0.1',
-       'scipy>=1.4.1',
+        'numpy>=1.18.1',
+        'matplotlib>=3.1.1',
+        'pandas>=1.0.1',
+        'scipy>=1.4.1',
+        'tqdm'
     ],
     ext_modules=cythonize(extensions)
 )
