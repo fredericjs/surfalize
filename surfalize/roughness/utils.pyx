aa3d1ae10e021d247e8f77afdb36daea036d8250
Frederic Schell <frederic.schell@iws.fraunhofer.de>
2024-01-09 23:05:57 +0100

    Added docstrings

--- surfalize/roughness/utils.pyx
+++ surfalize/roughness/utils.pyx
@@ -4,6 +4,21 @@ import numpy as np
 cimport numpy as np
 
 cpdef int argclosest(double[:] arr, double value):
+    """
+    Find the index of the closest value in a sorted array.
+
+    Parameters
+    ----------
+    arr : np.ndarray
+        The sorted array.
+    value : float
+        The target value.
+
+    Returns
+    -------
+    int
+        The index of the closest value.
+    """
     cdef int low, high, mid
     high = arr.size - 1
     low = 0
@@ -21,6 +36,21 @@ cpdef int argclosest(double[:] arr, double value):
 @cython.wraparound(False)
 @cython.cdivision(True)
 cdef double trapz1d(np.ndarray[np.float64_t, ndim=1] y, np.ndarray[np.float64_t, ndim=1] x):
+    """
+    Compute the trapezoidal integration of a 1D curve.
+
+    Parameters
+    ----------
+    y : np.ndarray[np.float64_t, ndim=1]
+        The y-values of the curve.
+    x : np.ndarray[np.float64_t, ndim=1]
+        The corresponding x-values.
+
+    Returns
+    -------
+    double
+        The trapezoidal integral of the curve.
+    """
     cdef double result = 0.0
     cdef Py_ssize_t i
     cdef Py_ssize_t size = x.size
@@ -34,6 +64,21 @@ cdef double trapz1d(np.ndarray[np.float64_t, ndim=1] y, np.ndarray[np.float64_t,
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef Py_ssize_t binary_search_ascending(double[:] arr, double target):
+    """
+    Perform a binary search on a sorted array in ascending order.
+
+    Parameters
+    ----------
+    arr : np.ndarray[np.float64_t, ndim=1]
+        The sorted array.
+    target : float
+        The target value.
+
+    Returns
+    -------
+    Py_ssize_t
+        The index of the target value or -1 if not found.
+    """
     cdef Py_ssize_t left = 0
     cdef Py_ssize_t right = arr.size - 1
     cdef Py_ssize_t result_index = -1
@@ -51,6 +96,21 @@ cpdef Py_ssize_t binary_search_ascending(double[:] arr, double target):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef Py_ssize_t binary_search_descending(double[:] arr, double target):
+    """
+    Perform a binary search on a sorted array in descending order.
+
+    Parameters
+    ----------
+    arr : np.ndarray[np.float64_t, ndim=1]
+        The sorted array.
+    target : float
+        The target value.
+
+    Returns
+    -------
+    Py_ssize_t
+        The index of the target value or -1 if not found.
+    """
     cdef Py_ssize_t left = 0
     cdef Py_ssize_t right = arr.size - 1
     cdef Py_ssize_t result_index = -1
@@ -68,6 +128,25 @@ cpdef Py_ssize_t binary_search_descending(double[:] arr, double target):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef class LinearInterpolator:
+    """
+    Linear interpolation class for 1D data.
+
+    Attributes
+    ----------
+    xdata : np.ndarray[np.float64_t, ndim=1]
+        The x-values of the data.
+    ydata : np.ndarray[np.float64_t, ndim=1]
+        The corresponding y-values of the data.
+    size : double
+        The size of the data.
+
+    Methods
+    -------
+    interpolate(x)
+        Perform linear interpolation for the given x-value.
+    get_index(x)
+        Get the index of the x-value in the sorted xdata.
+    """
     cdef double[:] xdata
     cdef double[:] ydata
     cdef double size
@@ -78,6 +157,20 @@ cdef class LinearInterpolator:
         self.size = xdata.size
 
     cpdef double interpolate(self, double x):
+        """
+        Perform linear interpolation for the given x-value.
+
+        Parameters
+        ----------
+        x : float
+            The x-value for interpolation.
+
+        Returns
+        -------
+        double
+            The interpolated y-value.
+        """
+
         cdef Py_ssize_t idx
         idx = self.get_index(x)
         cdef double x0 = self.xdata[idx]
@@ -95,6 +188,19 @@ cdef class LinearInterpolator:
 cdef class DescendingLinearInterpolator(LinearInterpolator):
 
     cpdef int get_index(self, double x):
+        """
+        Get the index of the x-value in the sorted xdata.
+
+        Parameters
+        ----------
+        x : float
+            The x-value.
+
+        Returns
+        -------
+        int
+            The index of the x-value.
+        """
         return binary_search_descending(self.xdata, x)
 
 @cython.boundscheck(False)
@@ -102,4 +208,17 @@ cdef class DescendingLinearInterpolator(LinearInterpolator):
 cdef class AscendingLinearInterpolator(LinearInterpolator):
 
     cpdef int get_index(self, double x):
+        """
+        Get the index of the x-value in the sorted xdata.
+
+        Parameters
+        ----------
+        x : float
+            The x-value.
+
+        Returns
+        -------
+        int
+            The index of the x-value.
+        """
         return binary_search_ascending(self.xdata, x)
