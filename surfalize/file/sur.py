8f2c8a69899977c39ef7fcdd36a21bd9683815ec
Frederic Schell <frederic.schell@iws.fraunhofer.de>
2024-03-27 16:41:05 +0100

    Implementing new SUR standard

--- surfalize/file/sur.py
+++ surfalize/file/sur.py
@@ -1,26 +1,80 @@
 import struct
+import zlib
 from datetime import datetime
-from .common import read_binary_layout, write_binary_layout, get_unit_conversion
-from ..exceptions import CorruptedFileError
+from enum import IntEnum
+from dataclasses import dataclass
+
 import numpy as np
+
+from .common import read_binary_layout, write_binary_layout, get_unit_conversion
+from ..exceptions import CorruptedFileError, UnsupportedFileFormatError
 
 # This is not fully implemented! Won't work with all SUR files.
 
-MAGIC = 'DIGITAL SURF'
+MAGIC_CLASSIC = 'DIGITAL SURF'
+MAGIC_COMPRESSED = 'DSCOMPRESSED'
 HEADER_SIZE = 512
 
+@dataclass
+class Directory:
+    len_raw_data: int
+    len_zipped_data: int
+
+@dataclass
+class SurObject:
+    header: dict
+    data: np.ndarray
+
+class StudiableType(IntEnum):
+    PROFILE = 1
+    SURFACE = 2
+    BINARY_IMAGE = 3
+    SERIES_OF_PROFILES = 4
+    SERIES_OF_SURFACES = 5
+    MERIDIAN_DISC = 6
+    MULTILAYER_PROFILE = 7
+    MULTILAYER_SURFACE = 8
+    PARALLEL_DISC = 9
+    INTENSITY_IMAGE = 10
+    INTENSITY_SURFACE = 11
+    RGB_IMAGE = 12
+    RGB_SURFACE = 13
+    FORCE_CURVE = 14
+    SERIES_OF_FORCE_CURVES = 15
+    RGB_INTENSITY_SURFACE = 16
+    PARAMETERIC_PROFILE = 17
+    SERIES_OF_RGB_IMAGES = 18
+    SPECTRUM_STUDIABLE = 19
+
+class AcquisitionType(IntEnum):
+    UNKNOWN = 0
+    CONTACT_STYLUS = 1
+    SCANNING_OPTICAL_GAUGE = 2
+    THERMOCOUPLE = 3
+    UNKNOWN_2 = 4
+    CONTACT_STYLUS_WITH_SKID = 5
+    AFM = 6
+    STM = 7
+    VIDEO = 8
+    INTERFEROMETER = 9
+    STRUCTURED_LIGHT_PROJECTION = 10
+
+
 LAYOUT_HEADER = (
-    ('code', '12s', False),
-    ('format', 'h', False),
+    ('code', '12s', False), # DIGITIAL SURF / DSCOMPRESSED
+    ('format', 'h', False), # 0 for PC format
     ('n_objects', 'h', False),
     ('version_number', 'h', False),
     ('studiable_type', 'h', True),
     ('name_object', '30s', True),
     ('name_operator', '30s', True),
-    (None, 6, None), # Reserved
+    ('p_size', 'h', True),
+    ('acquisition_type', 'h', True),
+    ('range_type', 'h', True),
     ('non_measured_points', 'h', False),
     ('absolute_z_axis', 'h', False),
-    (None, 8, None), # Reserved
+    ('gauge_resolution', 'f', True),
+    (None, 4, None), # Reserved
     ('bits_per_point', 'h', False),
     ('min_point', 'i', False),
     ('max_point', 'i', False),
@@ -54,7 +108,8 @@ LAYOUT_HEADER = (
     ('year', 'h', True),
     ('week_day', 'h', True),
     ('measurement_duration', 'f', True),
-    (None, 10, None), # Reserved
+    ('compressed_data_size', 'I', False),
+    (None, 6, None), # Reserved
     ('length_comment', 'h', False),
     ('length_private', 'h', False),
     ('client_zone', '128s', True),
@@ -67,9 +122,9 @@ LAYOUT_HEADER = (
     ('unit_step_t', '13s', True)
 )
 
-POINTSIZE = {16: 'h', 32: 'i'}
+DTYPE_MAP = {16: 'int16', 32: 'in32'}
 
-def write_sur(filepath, surface, encoding='utf-8'):
+def write_sur(filepath, surface, encoding='utf-8', compressed=False):
     INT32_MAX = int(2 ** 32 / 2) - 1
     INT32_MIN = -int(2 ** 32 / 2)
 
@@ -89,11 +144,11 @@ def write_sur(filepath, surface, encoding='utf-8'):
     timestamp = datetime.now()
 
     header = {
-        'code': MAGIC,
+        'code': MAGIC_CLASSIC if not compressed else MAGIC_COMPRESSED,
         'format': 0,  # PC Format
         'n_objects': 1,
         'version_number': 1,
-        'studiable_type': 2,
+        'studiable_type': StudiableType.SURFACE,
         'name_object': '',
         'name_operator': '',
         'non_measured_points': nm_points,
@@ -151,44 +206,105 @@ def write_sur(filepath, surface, encoding='utf-8'):
 
     with open(filepath, 'wb') as file:
         write_binary_layout(file, LAYOUT_HEADER, header)
-        data.tofile(file)
+        if not compressed:
+            data.tofile(file)
+            return
+        else:
+            uncompressed_data = data.tobytes()
+            compressed_data = zlib.compress(uncompressed_data)
+            # Write directory count = 1 and the length of a single data stream containing all the compressed data
+            file.write(struct.pack('<3I', 1, len(uncompressed_data), len(compressed_data)))
+            file.write(compressed_data)
+            return
+
+
+
+def read_sur_header(filehandle, encoding='utf-8'):
+    fp_start = filehandle.tell()
+    header = read_binary_layout(filehandle, LAYOUT_HEADER, encoding=encoding, fast=False)
+
+    if header['code'] not in (MAGIC_CLASSIC, MAGIC_COMPRESSED) or header['version_number'] != 1:
+        raise CorruptedFileError('Unknown header format')
+
+    if header['unit_ratio_x'] != 1 or header['unit_ratio_y'] != 1 or header['unit_ratio_z'] != 1:
+        raise NotImplementedError("This file type cannot be correctly read currently.")
+
+    header['studiable_type'] = StudiableType(header['studiable_type'])
+    header['acquisition_type'] = AcquisitionType(header['acquisition_type'])
+
+    if filehandle.tell() - fp_start != HEADER_SIZE:
+        raise CorruptedFileError("Unknown header size.")
+
+    header['comment'] = filehandle.read(header['length_comment'])
+    header['private'] = filehandle.read(header['length_private'])
+
+    if header['studiable_type'] not in [StudiableType.SURFACE, StudiableType.RGB_INTENSITY_SURFACE]:
+        raise UnsupportedFileFormatError(f'Studiables of type {header['studiable_type'].name} are not supported.')
+    if header['n_objects'] > 1:
+        raise UnsupportedFileFormatError(f'Multilayer or series studiables are not supported.')
+    return header
+
+def read_directory(filehandle):
+    return Directory(*struct.unpack('<2I', filehandle.read(8)))
+
+def read_compressed_data(filehandle, dtype):
+    dir_count = struct.unpack('I', filehandle.read(4))[0]
+    directories = []
+    for _ in range(dir_count):
+        directories.append(read_directory(filehandle))
+    decompressed_data = b''
+    for directory in directories:
+        compressed_data_stream = filehandle.read(directory.len_zipped_data)
+        decompressed_data_stream = zlib.decompress(compressed_data_stream)
+        if len(decompressed_data_stream) != directory.len_raw_data:
+            raise ValueError
+        decompressed_data += decompressed_data_stream
+    data = np.frombuffer(decompressed_data, dtype)
+    return data
+
+
+def read_sur_object(filehandle):
+    header = read_sur_header(filehandle)
+    dtype = DTYPE_MAP[header['bits_per_point']]
+    n_points = header['n_total_points']
+    ny = header['n_lines']
+    nx = header['n_points_per_line']
+    if header['code'] == MAGIC_CLASSIC:
+        data = np.fromfile(filehandle, count=n_points, dtype=dtype).reshape(ny, nx)
+    elif header['code'] == MAGIC_COMPRESSED:
+        data = read_compressed_data(filehandle, dtype).reshape(ny, nx)
+    else:
+        raise CorruptedFileError('Unknown header format')
+
+    if header['non_measured_points'] == 1:
+        invalidValue = header['min_point'] - 2
+        nan_mask = (data == invalidValue)
+
+    data = data * get_unit_conversion(header['unit_step_z'], 'um') * header['spacing_z']
+    step_x = get_unit_conversion(header['unit_step_x'], 'um') * header['spacing_x']
+    step_y = get_unit_conversion(header['unit_step_y'], 'um') * header['spacing_y']
+
+    if header['non_measured_points'] == 1:
+        data[nan_mask] = np.nan
+
+    data += header['offset_z']
+
+    return SurObject(header, data)
 
 def read_sur(filepath, encoding='utf-8'):
     filesize = filepath.stat().st_size
     with open(filepath, 'rb') as filehandle:
-        header = read_binary_layout(filehandle, LAYOUT_HEADER, encoding=encoding, fast=False)
+        while True:
+        sur_obj = read_sur_object(filehandle)
+        if sur_obj.header['studiable_type'] == StudiableType.SURFACE:
+            pass
 
-        if header['code'] != MAGIC or header['version_number'] != 1:
-            raise CorruptedFileError
 
-        if header['unit_ratio_x'] != 1 or header['unit_ratio_y'] != 1 or header['unit_ratio_z'] != 1:
-            raise NotImplementedError("This file type cannot be correctly read currently.")
 
-        filehandle.seek(header['length_comment'], 1)
-        filehandle.seek(header['length_private'], 1)
-        dtype = POINTSIZE[header['bits_per_point']]
-        dsize = struct.calcsize(dtype)
-        data_size = header['n_total_points'] * dsize
 
-        data_size = header['n_total_points'] * dsize
-        total_header_size = HEADER_SIZE + header['length_comment'] + header['length_private']
-        expected_data_size = header['n_total_points'] * dsize
-        if filesize - total_header_size > expected_data_size:
-            filehandle.seek(filesize - data_size, 0)
-        shape = (header['n_lines'], header['n_points_per_line'])
-        data = np.fromfile(filehandle, dtype=np.dtype(dtype)).reshape(shape)
 
-        if header['non_measured_points'] == 1:
-            invalidValue = header['min_point'] - 2
-            nan_mask = (data == invalidValue)
 
-        data = data * get_unit_conversion(header['unit_z'], 'um') * header['spacing_z']
-        step_x = get_unit_conversion(header['unit_x'], 'um') * header['spacing_x']
-        step_y = get_unit_conversion(header['unit_y'], 'um') * header['spacing_y']
 
-        data += header['offset_z']
 
-        if header['non_measured_points'] == 1:
-            data[nan_mask] = np.nan
 
         return (data, step_x, step_y)
