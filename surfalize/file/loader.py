e46fc9333a215886484b91485b5c2a97a1f7fa28
Samuel Schmidt <39338429+Sa-Schmi@users.noreply.github.com>
2024-03-28 16:12:53 +0100

    added sdf file

--- surfalize/file/loader.py
+++ surfalize/file/loader.py
@@ -10,28 +10,33 @@ from .zmg import read_zmg
 from .opd import read_opd
 from .xyz import read_xyz
 from .nms import read_nms
+from .sdf import read_binary_sdf
 
 dispatcher = {
-    '.sur': read_sur,
-    '.vk4': read_vk4,
-    '.vk6': read_vk6_vk7,
-    '.vk7': read_vk6_vk7,
-    '.plu': read_plu,
-    '.plux': read_plux,
-    '.zmg': read_zmg,
-    '.opd': read_opd,
-    '.xyz': read_xyz,
-    '.nms': read_nms
+    ".sur": read_sur,
+    ".vk4": read_vk4,
+    ".vk6": read_vk6_vk7,
+    ".vk7": read_vk6_vk7,
+    ".plu": read_plu,
+    ".plux": read_plux,
+    ".zmg": read_zmg,
+    ".opd": read_opd,
+    ".xyz": read_xyz,
+    ".nms": read_nms,
+    ".sdf": read_binary_sdf,
 }
 
 supported_formats = list(dispatcher.keys())
 
-def load_file(filepath, encoding='utf-8'):
+
+def load_file(filepath, encoding="utf-8"):
     filepath = Path(filepath)
     try:
         loader = dispatcher[filepath.suffix]
-        if 'encoding' in loader.__code__.co_varnames:
+        if "encoding" in loader.__code__.co_varnames:
             loader = partial(loader, encoding=encoding)
     except KeyError:
-        raise UnsupportedFileFormatError(f"File format {filepath.suffix} is currently not supported.")
+        raise UnsupportedFileFormatError(
+            f"File format {filepath.suffix} is currently not supported."
+        )
     return loader(filepath)
