ab59445cfb3b584c07fb39b91ee3a2db62811c9c
Frederic Schell <frederic.schell@iws.fraunhofer.de>
2024-04-29 09:08:50 +0200

    Added docstrings

--- surfalize/image.py
+++ surfalize/image.py
@@ -2,7 +2,10 @@ import PIL
 import numpy as np
 
 class Image:
-
+    """
+    Thin wrapper around numpy ndarray object representing an image.
+    Provides methods to show and save the image to disk.
+    """
     def __init__(self, data):
         self.data = data
 
@@ -11,11 +14,42 @@ class Image:
                 f'Mode: {"RGB" if self.data.ndim == 3 else "Grayscale"})')
 
     def save(self, path):
+        """
+        Save the image to disk using pillow.
+
+        Parameters
+        ----------
+        path: str | Path
+            Path where the image should be saved. The extension determines the file format.
+
+        Returns
+        -------
+        None
+        """
         PIL.Image.fromarray(self.data).save(path)
 
     def show(self):
+        """
+        Constructs a pillow image from the underlying array and shows in Jupyter.
+
+        Returns
+        -------
+        PIL.Image
+        """
         return PIL.Image.fromarray(self.data)
 
     @staticmethod
     def is_grayscale(array):
+        """
+        Determines whether a three dimensional array is grayscale by checking if all RGB channels have the same values.
+
+        Parameters
+        ----------
+        array: ndarray
+            ndarray of ndim 3.
+
+        Returns
+        -------
+        True if grayscale, else False
+        """
         return np.all((array[:, :, 0] == array[:, :, 1]) & (array[:, :, 0] == array[:, :, 2]))
