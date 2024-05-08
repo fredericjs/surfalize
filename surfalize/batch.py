Fixed bug in batch calculation

surfalize/batch.py
@@ -101,7 +101,11 @@ class Parameter:
         except CalculationError as error:
             if not ignore_errors:
                 raise error
-            result = [np.nan] * len(method.return_labels)
+            if hasattr(method, 'return_labels'):
+                length = len(method.return_labels)
+            else:
+                length = 1
+            result = [np.nan] * length
         if is_list_like(result):
             try:
                 labels = method.return_labels