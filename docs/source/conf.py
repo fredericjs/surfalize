3746bbe4139dff78fa90029549461317490284dd
Frederic Schell <frederic.schell@iws.fraunhofer.de>
2024-07-11 09:00:12 +0200

    Added HTML theme options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,4 +52,10 @@ autodoc_mock_imports = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
+html_theme_options = {
+    "sidebar_hide_name": False,
+    "navigation_with_keys": True,
+    # Add this line:
+    "navigation_depth": 3  # Adjust this number as needed
+}
 html_static_path = ['_static']
